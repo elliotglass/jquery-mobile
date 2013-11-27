c742bdcd955fd63f8feeaf223976916f3aac6c31
Ghislain Seguin <ghislain@gmail.com>
2013-11-27 10:33:22 -0700

    Release: Added issue tracker to release config

--- build/release.js
+++ build/release.js
@@ -1,6 +1,7 @@
 module.exports = function( Release ) {
 
 	Release.define({
+		issueTracker: "github",
 		changelogShell: function() {
 			return "# Changelog for jQuery Mobile v" + Release.newVersion + "\n";
 		}
